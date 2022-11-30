6518b33c04139d27b2aab852fdea51a5dad5916a
EmilioYbarra777 <gilbertoeybarrai@gmail.com>
2022-11-30 04:26:19 -0600

    reacomodo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use('/',express.static(__dirname + '/assets'));
 
